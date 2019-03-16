8ee78b6daf0d85e7d70c07b36330e2d7dc0d1679
Jake Ascher <ascherj@gmail.com>
2019-03-16 19:36:39 -0400

    Receive response from Google Maps API

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const { GOOGLE_API_KEY } = require('../config/google');
+
+const googleMapsClient = require('@google/maps').createClient({
+  key: GOOGLE_API_KEY,
+});
 
 // UNCOMMENT THE DATABASE YOU'D LIKE TO USE
 // var items = require('../database-mysql');
 // var items = require('../database-mongo');
 
 const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, '/../public')));
 
@@ -20,6 +29,18 @@ app.get('/locations', (req, res) => {
   // });
 });
 
-app.listen(3000, () => {
-  console.log('listening on port 3000!');
+app.get('/weather', (req, res) => {
+  const address = req.body.location;
+
+  googleMapsClient.geocode({ address }, (err, data) => {
+    const { lat, lng } = data.json.results[0].geometry.location;
+    console.log(`Address: ${address}`);
+    console.log(`Latitude: ${lat}`);
+    console.log(`Longitude: ${lng}`);
+    res.send('received location data');
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
 });
