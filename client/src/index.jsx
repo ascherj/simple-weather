f8cdf7aaadea22411ec4a0d7f071a1b211d316bc
Jake Ascher <ascherj@gmail.com>
2019-03-16 14:08:34 -0400

    Clean up codebase and refactor to use ES6 syntax

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,35 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
-import List from './components/List.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      items: []
-    }
+    this.state = {
+
+    };
   }
 
   componentDidMount() {
     $.ajax({
-      url: '/items', 
+      url: '/items',
       success: (data) => {
         this.setState({
-          items: data
-        })
+          items: data,
+        });
       },
       error: (err) => {
         console.log('err', err);
-      }
+      },
     });
   }
 
-  render () {
-    return (<div>
-      <h1>Item List</h1>
-      <List items={this.state.items}/>
-    </div>)
+  render() {
+    return (
+      <div>
+        <h1>Simple Weather</h1>
+      </div>
+    );
   }
 }
 
