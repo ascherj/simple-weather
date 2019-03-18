3a3bcad503ed6eae7d60c98da621d0254e3ee1c2
Jake Ascher <ascherj@gmail.com>
2019-03-18 11:31:47 -0400

    Refactor Google Maps API request and uninstall client library

--- server/index.js
+++ server/index.js
@@ -1,14 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const { GOOGLE_API_KEY } = require('../config/google');
-const { DARK_SKY_API_KEY } = require('../config/darksky');
-const utils = require('./utils');
-
-const googleMapsClient = require('@google/maps').createClient({
-  key: GOOGLE_API_KEY,
-  Promise: Promise
-});
+const { googleMapsRequest, darkSkyRequest } = require('./utils');
 
 // UNCOMMENT THE DATABASE YOU'D LIKE TO USE
 // var items = require('../database-mysql');
@@ -25,20 +18,19 @@ app.use(express.static(path.join(__dirname, '/../public')));
 app.get('/weather', (req, res) => {
   const address = req.query.location;
 
-  googleMapsClient.geocode({ address })
-    .asPromise()
+  googleMapsRequest(address)
     .then((response) => {
-      const { lat, lng } = response.json.results[0].geometry.location;
+      const { lat, lng } = response.data.results[0].geometry.location;
       console.log(`Location: ${address}`);
       console.log(`Latitude: ${lat}`);
       console.log(`Longitude: ${lng}`);
-      return utils.darkSkyRequest(lat, lng);
+      return darkSkyRequest(lat, lng);
     })
     .then((response) => {
       const forecast = response.data.currently;
       res.json(forecast);
     })
-    .catch((err) => {
+    .catch(() => {
       res.sendStatus(500);
     });
 });
--- server/utils.js
+++ server/utils.js
@@ -1,5 +1,17 @@
 const axios = require('axios');
-const { DARK_SKY_API_KEY } = require('../config/darksky');
+const { GOOGLE_API_KEY, DARK_SKY_API_KEY } = require('../config/keys');
+
+module.exports.googleMapsRequest = (address) => {
+  return new Promise((resolve, reject) => {
+    axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${GOOGLE_API_KEY}`)
+      .then((response) => {
+        resolve(response);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
 
 module.exports.darkSkyRequest = (lat, lng) => {
   return new Promise((resolve, reject) => {
