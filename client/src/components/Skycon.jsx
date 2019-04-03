8edfcc90e257e5bfb841bc51d9cd62757accf005
Jake Ascher <ascherj@gmail.com>
2019-04-03 15:39:52 -0400

    Add props validation to Skycon and refactor

--- client/src/components/Skycon.jsx
+++ client/src/components/Skycon.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Skycon = ({ icon }) => {
   const classes = !icon ? 'hidden' : '';
-  let skycons = new Skycons({ 'color': '#363636' });
+  const skycons = new Skycons({ color: '#363636' });
   skycons.add('skycon', icon);
   skycons.play();
 
   return (
     <canvas id="skycon" className={classes} width="128" height="128" />
   );
+};
+
+Skycon.propTypes = {
+  icon: PropTypes.string,
+};
+
+Skycon.defaultProps = {
+  icon: '',
 };
 
 export default Skycon;
